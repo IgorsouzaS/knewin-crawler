c15502b5b2451cabd2f809a00b7e5b95c10d14c5
Igor Souza <igorsouza2005@hotmail.com>
2020-08-21 01:50:40 +0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# knewin-crawler
+Projeto de web crawling em Java para captura de dados de notícias sobre o mercado financeiro.
---
+++ src/com/knewincrawler/KnewinCrawler.java
@@ -0,0 +1,12 @@
+package com.knewincrawler;
+
+public class KnewinCrawler {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
